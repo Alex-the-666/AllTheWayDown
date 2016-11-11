a24dfd579e50d04c211674e60f6ffb59dfcdc360
Alex-the-666 <alex.rowlands@cox.net>
2016-11-11 11:44:53 -0800

    basic mod

---
+++ src/main/java/com/github/alexthe666/allthewaydown/proxy/ClientProxy.java
@@ -0,0 +1,16 @@
+package com.github.alexthe666.allthewaydown.proxy;
+
+public class ClientProxy extends CommonProxy {
+
+        public void preinit(){
+
+        }
+
+        public void init(){
+
+        }
+
+        public void postinit(){
+
+        }
+}
---
+++ src/main/java/com/github/alexthe666/allthewaydown/proxy/CommonProxy.java
@@ -0,0 +1,16 @@
+package com.github.alexthe666.allthewaydown.proxy;
+
+public class CommonProxy {
+
+    public void preinit(){
+
+    }
+
+    public void init(){
+
+    }
+
+    public void postinit(){
+
+    }
+}
